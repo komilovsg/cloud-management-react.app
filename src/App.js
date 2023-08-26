42e10d811e1374a0eff8ebe22147dc95a2e72c40
komilovsg <komilovsg@gmail.com>
2023-08-26 23:25:56 +0500

    cloud-management.app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import About from './components/About';
+import AllInOne from './components/AllInOne';
+import Footer from './components/Footer';
+import Hero from './components/Hero';
+import Navbar from './components/Navbar'
+import Pricing from './components/Pricing';
+import Support from './components/Support'
 
 function App() {
   return (
-    <div className="">
-      
-    </div>
+    <>
+      <Navbar />
+      <Hero />
+      <About />
+      <Support />
+      <AllInOne />
+      <Pricing />
+      <Footer />
+    </>
   );
 }
 
